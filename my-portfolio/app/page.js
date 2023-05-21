036a578affad4aa418ba50c00172ccdb7eae5851
amaan-k <59571647+amaan-k@users.noreply.github.com>
2023-05-21 19:57:18 -0400

    Added initial files

---
+++ my-portfolio/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <section className='bg-blue-700 min-h-screen'>
+        <h1>Hello there</h1>
+      </section>
+    </main>
+  );
+}
