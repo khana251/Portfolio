d00307bdd73214f3702037c4b9c76947071204b5
amaan-k <59571647+amaan-k@users.noreply.github.com>
2023-05-22 22:06:27 -0400

    Removing conflicting files

--- app/page.js
+++ app/page.js
@@ -49,7 +49,7 @@ export default function Home() {
             </div>
             <div className='w-full md:w-1/2 flex justify-center items-center'>
               <div className='relative mt-30 mx-auto bg-gradient-to-b from-amber-100 rounded-full w-80 h-80 overflow-hidden'>
-                <Image src={avatar} fill style={{ objectFit: "cover" }} />
+                <Image src={avatar} alt='My icon' fill sizes="(max-width: 768px) 100vw" priority style={{ objectFit: "cover" }} />
               </div>
             </div>
           </div>
@@ -71,7 +71,7 @@ export default function Home() {
         </section>
         {/* Second page */} 
         <section>
-          <div className='px-20'>
+          <motion.div initial={{ opacity: 0 }} transition={{ duration: 1 }} whileInView={{ opacity: 1 }} className='px-20'>
             <h3 className='text-amber-100 text-3xl py-1'>Me Me Me</h3>
             <p className='text-amber-100 text-md md:text-lg py-2 leading-8 text-grey-800'>
               I have a strong proficiency in several programming languages, including Python, Java, C++, and SQL. Currently, I am expanding my skills in web development to pursue my passion for design, focusing on JavaScript, React, and Tailwind CSS. I have a keen interest in creating visually appealing websites and enjoy the process of bringing aesthetics and functionality together.
@@ -79,39 +79,65 @@ export default function Home() {
             <p className='text-amber-100 text-md md:text-lg py-2 leading-8 text-grey-800'>
               Last summer, I worked as a DevOps intern at Benevity, where I had the chance to work on several production issues that arose. This experience was invaluable in helping me gain confidence in my abilities and preparing me for a future full-time role.
             </p>
-          </div>
+          </motion.div>
+          <motion.div initial={{ opacity: 0 }} transition={{ duration: 1 }} whileInView={{ opacity: 1 }} className='p-10'>
+            <h2 className='text-5xl py-2 text-amber-100 font-medium md:text-9xl'>Some of my</h2>
+            <h2 className='text-center text-5xl py-2 text-amber-100 font-medium md:text-9xl'>s-·de projects</h2>
+          </motion.div>
           <div className='xl:flex gap-10 mx-auto'> 
             <div className='bg-amber-100 text-center shadow-lg p-10 rounded-xl my-10'>
-              <Image src={quizApp} wdith={100} height={100} className='mx-auto'/>
+              <Image src={quizApp} alt='Question Mark' wdith={100} height={100} className='mx-auto'/>
               <h3 className='text-lg font-medium pt-8 pb-2'>Quiz Application</h3>
               <p className='py-2'>
-                A quiz application to test your knowledge of basic to advanced github commands
+                A quiz application to test your knowledge of basic to advanced github commands.
               </p>
-              <h4 className='py-4 text-teal-600'>Tools I use</h4>
-              <p className='text-grey-800 py-1'>HTML</p>
-              <p className='text-grey-800 py-1'>CSS</p>
-              <p className='text-grey-800 py-1'>JacaScript</p>
-            </div>
-            <div className='bg-amber-100 text-center shadow-lg p-10 rounded-xl my-10'>
-              <Image src={minimalCountdownTimer} wdith={100} height={100} className='mx-auto'/>
-              <h3 className='text-lg font-medium pt-8 pb-2'>Minimal Countdown Timer</h3>
-              <p className='py-2'>
-                A timer to new year precisely to the second!
-              </p>
-              <h4 className='py-4 text-teal-600'>Tools I use</h4>
+              <h4 className='py-4 text-green-700'>Tools I used</h4>
               <p className='text-grey-800 py-1'>HTML</p>
               <p className='text-grey-800 py-1'>CSS</p>
               <p className='text-grey-800 py-1'>JavaScript</p>
+              <motion.ul className='px-4 py-4 flex flex-wrap justify-center gap-20'>
+                <motion.li whileHover={{ scale: 1.2 }}>
+                  <a className='bg-amber-200 px-4 py-2 text-black rounded-md' href='https://khana251.github.io/quiz-app/'>Demo</a>
+                </motion.li>
+                <motion.li whileHover={{ scale: 1.2 }}>
+                  <a className='bg-amber-200 px-4 py-2 text-black rounded-md' href='https://github.com/khana251/quiz-app'>Code</a>
+                </motion.li>
+              </motion.ul>
             </div>
             <div className='bg-amber-100 text-center shadow-lg p-10 rounded-xl my-10'>
-              <Image src={pong} wdith={100} height={100} className='mx-auto'/>
+              <Image src={pong} alt='Pong logo' wdith={100} height={100} className='mx-auto'/>
               <h3 className='text-lg font-medium pt-8 pb-2'>Pong Game Simulator</h3>
               <p className='py-2'>
                 A graphical game to simulate the classic pong game. Keeping track of scores as you play!
               </p>
-              <h4 className='py-4 text-teal-600'>Tools I use</h4>
+              <h4 className='py-4 text-green-700'>Tools I use</h4>
               <p className='text-grey-800 py-1'>Python</p>
               <p className='text-grey-800 py-1'>Turtle</p>
+              <p className='text-grey-800 py-1'>Graphics</p>
+              <motion.ul className='px-4 py-4 flex flex-wrap justify-center gap-2'>
+                <motion.li whileHover={{ scale: 1.2 }}>
+                  <a className='bg-amber-200 px-4 py-2 text-black rounded-md' href='https://github.com/khana251/pong'>Code</a>
+                </motion.li>
+              </motion.ul>
+            </div>
+            <div className='bg-amber-100 text-center shadow-lg p-10 rounded-xl my-10'>
+              <Image src={minimalCountdownTimer} alt='Timer logo' wdith={100} height={100} className='mx-auto'/>
+              <h3 className='text-lg font-medium pt-8 pb-2'>Minimal Countdown Timer</h3>
+              <p className='py-2'>
+                A clean and minimal countdown timer with the precise number of days, hours, minutes and seconds to new year
+              </p>
+              <h4 className='py-4 text-green-700'>Tools I used</h4>
+              <p className='text-grey-800 py-1'>HTML</p>
+              <p className='text-grey-800 py-1'>CSS</p>
+              <p className='text-grey-800 py-1'>JavaScript</p>
+              <motion.ul className='px-4 py-4 flex flex-wrap justify-center gap-2'>
+                <motion.li whileHover={{ scale: 1.2 }}>
+                  <a className='bg-amber-200 px-4 py-2 text-black rounded-md' href='https://khana251.github.io/Minimal-Countdown-Timer/'>Demo</a>
+                </motion.li>
+                <motion.li whileHover={{ scale: 1.2 }}>
+                  <a className='bg-amber-200 px-4 py-2 text-black rounded-md' href='https://github.com/khana251/Minimal-Countdown-Timer'>Code</a>
+                </motion.li>
+              </motion.ul>
             </div>
           </div>
         </section>
@@ -125,14 +151,6 @@ export default function Home() {
             <p className='text-md py-2 leading-8 text-grey-800 text-amber-100'>
               I have degree in <span className='text-teal-500'>computer science</span> from McMaster University with technical background in Development as a DevOps Developer prev @ Benevity
             </p>
-          </div>
-          <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-            <div className='basis-1/3 flex-1'><Image src={web1} className='rounded-lg object-cover' width={'100%'} height={'100%'} responsive/></div>
-            <div className='basis-1/3 flex-1'><Image src={web2} className='rounded-lg object-cover' width={'100%'} height={'100%'} responsive/></div>
-            <div className='basis-1/3 flex-1'><Image src={web3} className='rounded-lg object-cover' width={'100%'} height={'100%'} responsive/></div>
-            <div className='basis-1/3 flex-1'><Image src={web4} className='rounded-lg object-cover' width={'100%'} height={'100%'} responsive/></div>
-            <div className='basis-1/3 flex-1'><Image src={web5} className='rounded-lg object-cover' width={'100%'} height={'100%'} responsive/></div>
-            <div className='basis-1/3 flex-1'><Image src={web6} className='rounded-lg object-cover' width={'100%'} height={'100%'} responsive/></div>
           </div>
         </section>
       </main>
